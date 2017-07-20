a73cd96c7e73030af9638837d8d96ffeed83b84d
kakts <kakts.git@gmail.com>
2017-07-21 00:47:32 +0900

    Chapter5 add http server

---
+++ chapter5/index.js
@@ -0,0 +1,19 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+// Add routing
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: function(request, reply) {
+    console.log('[/hello] requested');
+    reply('hello world');
+  }
+});
+
+server.start();
